357c6d3d4a56b1c327bb237c6d360b5bcb3146ec
andre <andre.muehle@outlook.com>
2021-03-02 16:37:36 +0100

    fix service startup

--- API.py
+++ API.py
@@ -41,4 +41,4 @@ def index():
     response.content_type = 'application/json'
     return json.dumps(objs, default=tojson)
 
-run(host='localhost', port=8080, debug=True, reloader=True)
+run(host='0.0.0.0', port=8080, debug=True, reloader=True)
